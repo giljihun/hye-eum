ab6a9882aa910eacac0da8609691754e5c4579a1
giljihun <accc45@outlook.com>
2024-05-09 20:43:09 +0900

    feat(appdelegate) : 세로방향 고정

--- hye:eum/AppDelegate.swift
+++ hye:eum/AppDelegate.swift
@@ -31,6 +31,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+           
+           // 세로방향 고정
+           return UIInterfaceOrientationMask.portrait
+       }
 
     // MARK: - Core Data stack
 
